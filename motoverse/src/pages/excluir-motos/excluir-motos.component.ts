c4df6021b5cf1378ef34898c2713e8497467a7ca
cat <148160989+catacastanha@users.noreply.github.com>
2025-05-13 20:26:39 -0300

    feat: página de cadastro
    - Criação da página de cadastro

--- motoverse/src/pages/excluir-motos/excluir-motos.component.ts
+++ motoverse/src/pages/excluir-motos/excluir-motos.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-excluir-motos',
   imports: [],
-  templateUrl: './excluir-motos.component.html',
-  styleUrl: './excluir-motos.component.css'
+  templateUrl: './excluir-motos.component.html'
 })
 export class ExcluirMotosComponent {
 
