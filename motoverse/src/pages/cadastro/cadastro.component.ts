4d5e18eaec1ee8c40bf8644aa8f2214bac2d38c7
VianaDavi <148826598+VianaDavi@users.noreply.github.com>
2025-05-13 22:10:45 -0300

    feat: visualizar a senha digitada
    - Feita a criação de um checkbox que possibilita o usuário visualizar ou não a senha digitada.

--- motoverse/src/pages/cadastro/cadastro.component.ts
+++ motoverse/src/pages/cadastro/cadastro.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
+  standalone: true,
   imports: [RouterModule, RouterLink],
   templateUrl: './cadastro.component.html',
 })
-export class CadastroComponent {
 
+export class CadastroComponent implements AfterViewInit {
+  @ViewChild('inputSenha') inputSenhaRef!: ElementRef<HTMLInputElement>;
+  @ViewChild('toggleSenha') toggleSenhaRef!: ElementRef<HTMLInputElement>;
+
+  ngAfterViewInit(): void {
+    const inputSenha = this.inputSenhaRef.nativeElement;
+    const toggleSenha = this.toggleSenhaRef.nativeElement;
+
+    inputSenha.type = 'password';
+
+    toggleSenha.addEventListener('change', () => {
+      inputSenha.type = toggleSenha.checked ? 'text' : 'password';
+    });
+  }
 }
