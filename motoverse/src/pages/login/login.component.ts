19b408b5a62ffc6d37df55bd87e283bcce33242a
cat <148160989+catacastanha@users.noreply.github.com>
2025-05-15 20:25:08 -0300

    feat: script para formatar inputs
    - criação de uma mascara para número de celular
    - criação de uma mascara para cpf

--- motoverse/src/pages/login/login.component.ts
+++ motoverse/src/pages/login/login.component.ts
@@ -8,17 +8,31 @@ import { RouterLink, RouterModule } from '@angular/router';
 })
 export class LoginComponent implements AfterViewInit {
   @ViewChild('inputSenha') inputSenhaRef!: ElementRef<HTMLInputElement>;
-  @ViewChild('toggleSenha') toggleSenhaRef!: ElementRef<HTMLInputElement>;
+  @ViewChild('toggleSenha') mostraSenhaRef!: ElementRef<HTMLInputElement>;
+  @ViewChild('inputCpf') inputCpfRef!: ElementRef<HTMLInputElement>;
 
   ngAfterViewInit(): void {
     const inputSenha = this.inputSenhaRef.nativeElement;
-    const toggleSenha = this.toggleSenhaRef.nativeElement;
+    const mostraSenha = this.mostraSenhaRef.nativeElement;
 
     inputSenha.type = 'password';
 
-    toggleSenha.addEventListener('change', () => {
-      inputSenha.type = toggleSenha.checked ? 'text' : 'password';
+    mostraSenha.addEventListener('change', () => {
+      inputSenha.type = mostraSenha.checked ? 'text' : 'password';
+    });
+
+    const inputCpf = this.inputCpfRef.nativeElement;
+
+    inputCpf.addEventListener('input', () => {
+      let valor = inputCpf.value.replace(/\D/g, '');
+      if (valor.length > 11) valor = valor.substring(0, 11);
+
+      //000.000.000-00
+      valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+      valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+      valor = valor.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+
+      inputCpf.value = valor;
     });
   }
 }
-
