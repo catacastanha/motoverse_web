2896a142d33c7dd9b95ee2ba1755518e74b84d12
Davi Viana <148826598+VianaDavi@users.noreply.github.com>
2025-05-27 20:35:27 -0300

    feat: página de excluir motos
    - Criada a página de exclusão de motos do estoque para administradores

--- motoverse/src/pages/cadastro-produtos/cadastro-produtos.component.ts
+++ motoverse/src/pages/cadastro-produtos/cadastro-produtos.component.ts
@@ -1,66 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-produtos',
-  imports: [RouterLink, CommonModule],
+  imports: [],
   templateUrl: './cadastro-produtos.component.html',
   styleUrl: './cadastro-produtos.component.css'
 })
 export class CadastroProdutosComponent {
 
-  motos = [
-    {
-      id: 1,
-      nome: 'Honda CG 160 Titan 2025',
-      imagem: '/images-estoque/cg160.png',
-      precoAntigo: 21100,
-      precoAtual: 19556,
-      km: 0,
-      ano: 2025,
-    },
-    {
-      id: 2,
-      nome: 'Honda XRE 190 2025',
-      imagem: '/images-estoque/xre190.png',
-      precoAntigo: 24460,
-      precoAtual: 23913,
-      km: 0,
-      ano: 2025,
-    },
-    {
-      id: 3,
-      nome: 'Yamaha MT-03 2025',
-      imagem: '/images-estoque/mt03.png',
-      precoAntigo: 35700,
-      precoAtual: 33920.90,
-      km: 0,
-      ano: 2025,
-    },
-    {
-      id: 4,
-      nome: 'Bajaj Dominar 400 2025',
-      imagem: '/images-estoque/dominar400.png',
-      precoAtual: 26350,
-      km: 0,
-      ano: 2025,
-    },
-    {
-      id: 5,
-      nome: 'BMW GS 310 2025',
-      imagem: '/images-estoque/310gs.png',
-      precoAtual: 39500,
-      km: 0,
-      ano: 2025,
-    },
-    {
-      id: 6,
-      nome: 'Triumph Tiger 1200 2025',
-      imagem: '/images-estoque/tiger1200.png',
-      precoAtual: 88990,
-      km: 0,
-      ano: 2025
-    },
-  ];
 }
