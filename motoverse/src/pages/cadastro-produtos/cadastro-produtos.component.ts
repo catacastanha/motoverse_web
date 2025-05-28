04715b981f2f175598686542b190b1dec54461e6
cat <148160989+catacastanha@users.noreply.github.com>
2025-05-27 22:07:30 -0300

    feat: paginas finalizadas

--- motoverse/src/pages/cadastro-produtos/cadastro-produtos.component.ts
+++ motoverse/src/pages/cadastro-produtos/cadastro-produtos.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-produtos',
-  imports: [],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './cadastro-produtos.component.html',
   styleUrl: './cadastro-produtos.component.css'
 })
